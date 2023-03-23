0b2aef3cd6323564d9a62d52ef8aca10ef3d513f
Ricky K <riickyy_kvarn@hotmail.se>
2023-03-23 14:05:34 +0100

    added dnd

--- src/components/Ticket/Ticket.tsx
+++ src/components/Ticket/Ticket.tsx
@@ -1,14 +1,24 @@
 import React from "react";
 import { Ticket as TicketType } from "../../context/projects-data";
 import "./Ticket.scss";
+import { DraggableProvided, DraggableStateSnapshot } from "react-beautiful-dnd";
 
 type Props = {
   ticket: TicketType;
+  provided: DraggableProvided;
+  snapshot: DraggableStateSnapshot;
 };
 
-export default function Ticket({ ticket }: Props) {
+export default function Ticket({ ticket, provided, snapshot }: Props) {
   return (
-    <div className="ticket">
+    <div
+      className="ticket"
+      ref={provided.innerRef}
+      {...provided.draggableProps}
+      {...provided.dragHandleProps}
+      //@ts-ignore
+      active={snapshot.isDragging.toString()}
+    >
       <h3>{ticket.title}</h3>
       <p>{ticket.description}</p>
       <p>{ticket.tasks.length}</p>
